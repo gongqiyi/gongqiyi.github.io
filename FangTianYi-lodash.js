2996ce87407e44da5c3f5b6005febb80169e7fc7
gongqiyi <474238960@qq.com>
2016-11-17 01:04:52 +0800

    Update FangTianYi-lodash.js

--- FangTianYi-lodash.js
+++ FangTianYi-lodash.js
@@ -1,6 +1,21 @@
 var FangTianYi = {
-	chunk: function() {
+	chunk: function(arr, n) {
 
+		var l = Math.ceil(arr.length / n)
+
+		var result = new Array(l);
+
+		for (var i = 0; i < l; i++) {
+			result[i] = []
+		}
+
+
+
+		for (var i = 0; i < arr.length; i++) {
+			result[parseInt(i / n)][i % n] = arr[i]
+		}
+
+		return result
 	},
 	compact: function(arr) {
 		for (var i = 0; i < arr.length; i++) {
@@ -11,5 +26,9 @@ var FangTianYi = {
 
 		}
 		return arr //函数一定要return
-	}
+	},
+	drop: function(arr, n) {
+		arr.splice(0, n)
+		return arr
+	},
 }
